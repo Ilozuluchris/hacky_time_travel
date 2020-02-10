9c47e2142bb13c03079608a0386e6bb2cd5fc75e
ilozuluchris <ilozuluchidiuso@gmail.com>
2020-02-10 02:34:44 +0100

    testing the get data func

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 import requests
+
+def getData():
+    website_url = "https://www.onthisday.com/"
+    res = requests.get(website_url)
+    if res.status_code == 200:
+        return {'status': True, "data": res.text}
+    else:
+        return {'status': False}
